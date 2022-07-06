f97ecac8b12c4e6e3a772bf61ad812cf3bbe0b35
hoyeol0513 <hoyeol0513@naver.com>
2022-07-06 12:07:36 +0900

    doDelete

--- src/main/java/com/mysite/sbb/article/controller/ArticleController.java
+++ src/main/java/com/mysite/sbb/article/controller/ArticleController.java
@@ -34,18 +34,23 @@ public class ArticleController {
     @RequestMapping("/doModify")
     @ResponseBody
     public Article showModify(long id, String title, String body){
-        //get -> 수정
+        //get -> 조건에 맞는 데이터 가져오기
         Article article = articleRepository.findById(id).get();
         if(title != null){
-            article.setTitle(title);
+            article.setTitle(title); //불러온 데이터 수정
         }
         if(body != null){
-            article.setBody(body);
+            article.setBody(body); //불러온 데이터 수정
         }
         // 수정된 정보를 db로 저장
         articleRepository.save(article);
         return article;
     }
-
+    @RequestMapping("/doDelete")
+    @ResponseBody
+    public String doDelete(long id){
+        articleRepository.deleteById(id); //id로 찾아서 삭제
+        return "%d번 게시물이 삭제되었습니다.".formatted(id);
+    }
 
 }
